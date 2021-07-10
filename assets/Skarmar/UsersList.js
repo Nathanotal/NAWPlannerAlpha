a55f9bcc1e746edfe9f4581e8a210d1e24aa9cba
Nathanotal <Nathan.allard@gmail.com>
2021-07-10 13:59:20 +0200

    Reimplemented firebase, added schema, dealt with keys

---
+++ assets/Skarmar/UsersList.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import colors from "../colors";
+import firebase from "../../firebase";
+
+
+// Fix how values are handled!
+function Userslist({ navigation }) {
+  const [isLoading, setLoadStatus] = useState(true); // Fix this later!
+  const [users, setUsers] = useState([]);
+  const ref = firebase.firestore().collection("users");
+
+  useEffect(() => {
+    getUsers();
+  });
+
+  function getUsers() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc)=> {
+        items.push(doc.data());
+      });
+      setUsers(items)
+    })
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <div>
+        <h1>Users</h1>
+        {users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.desc}</p>
+          </div>
+        ))}
+      </div>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+});
+
+export default Userslist;
---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import keys from "./keys.json"
+
+const firebaseConfig = {
+  apiKey: keys.keys.APP_FIREBASE_KEY,
+  authDomain: keys.keys.APP_FIREBASE_DOMAIN,
+  databaseURL: keys.keys.APP_FIREBASE_DATABASE,
+  projectId: keys.keys.APP_FIREBASE_PROJECTID,
+  storageBucket: keys.keys.APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: keys.keys.APP_FIREBASE_SENDER_ID,
+  appId: keys.keys.APP_FIREBASE_APP_ID,
+  measurementId: keys.keys.APP_FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig); 
+
+export default firebase;

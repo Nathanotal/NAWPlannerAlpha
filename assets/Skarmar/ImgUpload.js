358d3b1a77eccac8a8a447a5bbccacce2596523d
Nathanotal <Nathan.allard@gmail.com>
2021-07-16 18:00:01 +0200

    x

---
+++ assets/Skarmar/ImgUpload.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+import keys from "./keys.json";
+
+// Imgur, put on hold
+function ImgUpload(props) {
+  return (
+    <View>
+      <Text>IMGUPLOAD</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ImgUpload;
